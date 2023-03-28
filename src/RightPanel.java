c5ec2b1dcafc2b98e6b44103a489825476d34311
PorshinRuslan <porshin.rus@bk.ru>
2023-03-28 15:34:28 +0400

    version 2.0

--- src/RightPanel.java
+++ src/RightPanel.java
@@ -1,18 +1,28 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class RightPanel extends JPanel {
 
-    public RightPanel(){
+    ArrayList<JPanel> list = new ArrayList();
 
+    public RightPanel(){
         setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
-//        setLayout(new GridLayout(3,1));
+        list.add(new TopPanel());
+        list.add(new DatePanel(3,2023));
+        list.add(new DownPanel());
         init();
     }
 
     public void init(){
-        add(new TopPanel());
-        add(new DatePanel(3,2023));
-        add(new DownPanel());
+
+        for(int i=0; i<list.size(); i++){
+            add(list.get(i));
+        }
+
+//        add(new TopPanel());
+//        add(new DatePanel(3,2023));
+//        add(new DownPanel());
+
     }
 }
--- src/TopPanel.java
+++ src/TopPanel.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TopPanel extends JPanel {
 
     Bottom[] mass_b = new Bottom[4];
-    String[] s = {"Mounth", "<","year",">"};
+    String[] s = {"Year", "<","Month",">"};
+    int year;
+    int month;
 
     public TopPanel(){
 
@@ -12,6 +16,8 @@ public class TopPanel extends JPanel {
         setMaximumSize(new Dimension(400,50));
         setLayout(new GridLayout(1,4));
         init();
+        thisDate();
+        showDate();
     }
 
     public void init(){
@@ -20,5 +26,21 @@ public class TopPanel extends JPanel {
             add(mass_b[i]);
             mass_b[i].setText(s[i]);
         }
+    }
+
+    public void thisDate(){
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+    }
+
+    public void showDate(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        SimpleDateFormat sd_month = new SimpleDateFormat("MMMM");
+        SimpleDateFormat sd_year = new SimpleDateFormat("yyyy");
+        mass_b[0].setText(sd_year.format(calendar.getTime()));
+        mass_b[2].setText(sd_month.format(calendar.getTime()));
     }
 }
